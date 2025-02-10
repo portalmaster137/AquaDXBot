945add68a09218f1d01efa339e280a2c2add93a4
Porta <portalmaster137@gmail.com>
2025-02-10 10:07:26 +0000

    Add command registration and update example environment file

---
+++ src/commandRegister.ts
@@ -0,0 +1,27 @@
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
+import { configDotenv } from "dotenv";
+import validateEnv from "./envValidator";
+import { LogSingleton } from "./Globals";
+configDotenv();
+validateEnv();
+
+const commands = [
+    new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with Pong!"),
+].map(command => command.toJSON());
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN as string);
+
+(async () => {
+    try {
+        LogSingleton.getInstance().info("Started refreshing application (/) commands.");
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID as string, process.env.GUILD_ID as string),
+        )
+        LogSingleton.getInstance().info("Successfully reloaded application (/) commands.", {body: data});
+    }
+    catch (error) {
+        LogSingleton.getInstance().error(error);
+    }
+})()
