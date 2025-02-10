f2a9810650b69838366727d8fc3f7c3521662cbd
Porta <portalmaster137@gmail.com>
2025-02-11 04:46:52 +1000

    feat: Add option to wipe old commands before refreshing application commands

--- src/commandRegister.ts
+++ src/commandRegister.ts
@@ -16,15 +16,21 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN as string)
         LogSingleton.getInstance().info(
             "Started refreshing application (/) commands."
         )
+
+        if (process.env.WIPE_OLD_COMMANDS === "true") {
+            await rest.put(Routes.applicationCommands(process.env.CLIENT_ID as string), {body : []})
+            await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID as string, process.env.GUILD_ID as string), {body : []})
+        }
+
         const data = await rest.put(
             Routes.applicationGuildCommands(
                 process.env.CLIENT_ID as string,
                 process.env.GUILD_ID as string
-            )
+            ),
+            { body: commands }
         )
         LogSingleton.getInstance().info(
-            "Successfully reloaded application (/) commands.",
-            { body: data }
+            "Successfully reloaded application (/) commands."
         )
     } catch (error) {
         LogSingleton.getInstance().error(error)
