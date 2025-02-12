ab8efc4aa838d9d3e85a6ceebed3a5e9731dabad
Paiton Bertschy <paitonbertschy@gmail.com>
2025-02-11 19:18:27 -0600

    feat: :sparkles: Enhance GamesCommand to provide detailed game information based on user selection

--- src/interactions/games.ts
+++ src/interactions/games.ts
@@ -1,15 +1,32 @@
-import { ChatInputCommandInteraction, Interaction } from "discord.js"
-import ISlashCommand from "./general"
+import { ChatInputCommandInteraction } from "discord.js";
+import ISlashCommand from "./general";
 
 export default class GamesCommand implements ISlashCommand {
-    id = "games"
+    id = "games";
     
     async execute(interaction: ChatInputCommandInteraction) {
         const game = interaction.options.getString('game');
+        let replyMessage = "";
+
         if (game) {
-            interaction.reply(`You selected the game: ${game}`);
+            switch (game) {
+                case 'chuni':
+                    replyMessage = "You selected Chunithm! Chunithm is a rhythm game developed by Sega. Latest Supported Version: Luminous Plus (2.27)";
+                    break;
+                case 'mai2':
+                    replyMessage = "You selected maimai! maimai is a rhythm game developed by Sega. Latest Supported Version: maimai DX Prism (1.50) ";
+                    break;
+                case 'mu3':
+                replyMessage = "You selected Ongeki! Ongeki is a rhythm game developed by Sega. Latest Supported Version: Ongeki Bright Memories (1.45)";
+                    break;
+                default:
+                    replyMessage = "Unknown game selected.";
+                    break;
+            }
         } else {
-            interaction.reply("No game selected.");
+            replyMessage = "No game selected.";
         }
+
+        await interaction.reply(replyMessage);
     }
 }
