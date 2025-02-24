2222da61305d3ab8e6b6053ae9f3af346b1a73e0
Paiton Bertschy <78337764+thewiilover@users.noreply.github.com>
2025-02-23 22:04:46 -0600

    refactor: :sparkles: Simplify game response handling in GamesCommand class

--- src/interactions/games.ts
+++ src/interactions/games.ts
@@ -1,68 +1,39 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import ISlashCommand from "./general";
-
-export default class GamesCommand implements ISlashCommand {
+const gameResponses: {
+    [game: string]: {
+      [option: string]: string;
+      default: string;
+    };
+  } = {
+    chuni: {
+      oldest: "Chunithm oldest supported version: Chunithm NEW!! (2.00)",
+      newest: "Chunithm newest supported version: Chunithm VERSE (2.30)",
+      faq: "Chunithm FAQ...",
+      default: "Unknown option selected for Chunithm."
+    },
+    mai2: {
+      oldest: "maimai oldest supported version: maimai DX (1.00)",
+      newest: "maimai newest supported version: maimai DX Prism (1.50)",
+      faq: "maimai FAQ...",
+      default: "Unknown option selected for maimai."
+    },
+    mu3: {
+      oldest: "Ongeki oldest supported version: O.N.G.E.K.I. (1.00)",
+      newest: "Ongeki newest supported version: O.N.G.E.K.I. bright MEMORY (1.40)",
+      faq: "Ongeki FAQ...",
+      default: "Unknown option selected for Ongeki."
+    }
+  };
+  
+  export default class GamesCommand implements ISlashCommand {
     id = "games";
-    
+  
     async execute(interaction: ChatInputCommandInteraction) {
-        const subcommand = interaction.options.getSubcommand();
-        const option = interaction.options.getString('option');
-        let replyMessage = "";
-
-        switch (subcommand) {
-            case 'chuni':
-                replyMessage = this.handleChuni(option);
-                break;
-            case 'mai2':
-                replyMessage = this.handleMai2(option);
-                break;
-            case 'mu3':
-                replyMessage = this.handleMu3(option);
-                break;
-            default:
-                replyMessage = "Unknown game selected.";
-                break;
-        }
-
-        await interaction.reply(replyMessage);
+      const subcommand = interaction.options.getSubcommand();
+      const option = interaction.options.getString('option');
+      const game = gameResponses[subcommand];
+      const replyMessage = game ? (option && game[option] || game.default) : "Unknown game selected.";
+      await interaction.reply(replyMessage);
     }
-
-    handleChuni(option: string | null): string {
-        switch (option) {
-            case 'oldest':
-                return "Chunithm oldest supported version: Chunithm NEW!! (2.00)";
-            case 'newest':
-                return "Chunithm newest supported version: Chunithm LUMINOUS PLUS (2.27)";
-            case 'faq':
-                return "Chunithm FAQ...";
-            default:
-                return "Unknown option selected for Chunithm.";
-        }
-    }
-
-    handleMai2(option: string | null): string {
-        switch (option) {
-            case 'oldest':
-                return "maimai oldest supported version: maimai DX (1.00)";
-            case 'newest':
-                return "maimai newest supported version: maimai DX Prism (1.50)";
-            case 'faq':
-                return "maimai FAQ...";
-            default:
-                return "Unknown option selected for maimai.";
-        }
-    }
-
-    handleMu3(option: string | null): string {
-        switch (option) {
-            case 'oldest':
-            return "Ongeki oldest supported version: O.N.G.E.K.I. (1.00)";
-            case 'newest':
-                return "Ongeki newest supported version: O.N.G.E.K.I. bright MEMORY (1.40)";
-            case 'faq':
-                return "Ongeki FAQ...";
-            default:
-                return "Unknown option selected for Ongeki.";
-        }
-    }
-}
+  }
