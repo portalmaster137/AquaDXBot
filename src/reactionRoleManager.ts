0b4a5a5a0044255730997a1fa48952de890e093e
thewiilover <bertspai000@student.valders.k12.wi.us>
2025-03-02 14:03:24 -0600

    feat: implement reaction role manager setup in reactionRoleManager.ts

---
+++ src/reactionRoleManager.ts
@@ -0,0 +1,15 @@
+import { Client, Snowflake } from "discord.js"
+import { ReactionRole, ReactionRoleConfiguration } from "discordjs-reaction-role
+
+export const setupReactionRoleManager = (client: Client) => {
+    const MESSAGE: Snowflake = process.env.MESSAGE!!;
+    const ROLE: Snowflake = process.env.ROLE!!;
+    const configuration: ReactionRoleConfiguration[] = [
+        {
+            messageId: MESSAGE,
+            reaction: "🔴",
+            roleId: ROLE,
+        },
+    ];
+    return new ReactionRole(client, configuration);
+};
