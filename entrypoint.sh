b3f706db7605e1b63f4cf735db5dfe064156d1e8
thewiilover <bertspai000@student.valders.k12.wi.us>
2025-03-05 18:45:22 -0600

    fix: remove unnecessary wait time in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Waiting for database to be ready..."
-sleep 10  # Give the database container a bit more time
-
 echo "Running migrations..."
 bunx prisma migrate dev -n init
 echo "Starting application..."
